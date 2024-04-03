f11de715f58aab007319c20005e0c994462cce6f
Aryan Gupta <guptaaryan926@gmail.com>
2024-04-04 00:55:41 +0530

    added music download functionality

--- src/main/java/com/musicplayer/musicplayerbackend/Controllers/MusicController.java
+++ src/main/java/com/musicplayer/musicplayerbackend/Controllers/MusicController.java
@@ -2,12 +2,16 @@ package com.musicplayer.musicplayerbackend.Controllers;
 
 import com.musicplayer.musicplayerbackend.Service.GridFsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/api/v1/music")
@@ -28,5 +32,19 @@ public class MusicController {
         } catch (IOException e) {
             return new ResponseEntity<>("Failed to upload file: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<Resource> getMusicFile(@PathVariable("fileName") String fileName) throws IOException {
+        // Retrieve the music file content as an InputStream
+        InputStream inputStream = gridFsService.downloadFile(fileName);
+
+        // Create a Resource object to wrap the InputStream
+        InputStreamResource resource = new InputStreamResource(inputStream);
+
+        // Build a ResponseEntity with the resource and content type
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 }
--- src/main/java/com/musicplayer/musicplayerbackend/Service/GridFsService.java
+++ src/main/java/com/musicplayer/musicplayerbackend/Service/GridFsService.java
@@ -4,12 +4,16 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.gridfs.GridFSBucket;
 import com.mongodb.client.gridfs.GridFSBuckets;
+import com.mongodb.client.gridfs.model.GridFSFile;
 import com.mongodb.client.gridfs.model.GridFSUploadOptions;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -24,6 +28,7 @@ public class GridFsService {
     private final GridFSBucket gridFSBucket;
 
     public void uploadMusic(MultipartFile file, String bucketName) throws IOException {
+        //upload file using input stream
         InputStream inputStream = file.getInputStream();
         try {
             Document metaData = new Document();
@@ -34,5 +39,18 @@ public class GridFsService {
         } finally {
             inputStream.close();
         }
+    }
+
+    public InputStream downloadFile(String fileName) throws IOException{
+        GridFSFile gridFSFile = gridFSBucket.find(new Document("filename", fileName)).first();
+
+        if (gridFSFile == null){
+            throw new FileNotFoundException("File does not exist");
+        }
+
+        //get file by ID
+        ObjectId objectId = gridFSFile.getObjectId();
+
+        return gridFSBucket.openDownloadStream(objectId);
     }
 }
